f63d96d225be287a5c834ee2aa1eebaef48acd07
ajibade.mus <ajibade.musa@fcmb.com>
2023-02-09 13:23:30 +0100

    changed rest template to web client

--- employee-service/src/main/java/com/musdon/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/musdon/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -10,13 +10,14 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
-    private RestTemplate restTemplate;
+    private WebClient webClient;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -41,10 +42,14 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public APiResponseDto getEmployeeById(Long employeeId) {
         Employee employee = employeeRepository.findById(employeeId).get();
-        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(),
-                DepartmentDto.class);
-        DepartmentDto departmentDto = responseEntity.getBody();
+//        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(),
+//                DepartmentDto.class);
+//        DepartmentDto departmentDto = responseEntity.getBody();
 
+        DepartmentDto departmentDto = webClient.get().uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
         EmployeeDto employeeDto = EmployeeDto.builder()
                 .id(employee.getId())
                 .firstName(employee.getFirstName())
